bc6f2fd6f36d8eafdad2b37c0579ee1ff3d12331
KatlegoKhumalo <106166268+KatlegoKhumalo@users.noreply.github.com>
2025-09-26 11:10:47 +0200

    Delete AfricAI-Full-Build directory

--- AfricAI-Full-Build/frontend/components/icons/AwardIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { LucideIcon, type LucideIconProps } from './LucideIcon';
-
-export const AwardIcon: React.FC<LucideIconProps> = (props) => (
-    <LucideIcon {...props}>
-        <circle cx="12" cy="8" r="7" />
-        <polyline points="8.21 13.89 7 22 12 17 17 22 15.79 13.88" />
-    </LucideIcon>
-);
